8feadd056086ac2651fc4777f1bf775bb0e6a034
Pawson1988 <pawson1988@me.com>
2021-12-26 23:13:05 +0100

    added conditional background gradients

--- 3PalindromeChecker/app.js
+++ 3PalindromeChecker/app.js
@@ -3,20 +3,24 @@ const input = document.querySelector("#pal");
 const checkBtn = document.querySelector(".checkBtn");
 const result = document.querySelector(".result");
 
+const cls = ["isPalindrome", "isNotPalindrome", "defaultBackground"];
+
 checkBtn.addEventListener("click", () => {
   let value = input.value;
+  body.classList.remove(...cls);
   if (value) {
     let answer = isPalindrome(value);
     if (answer === true) {
       result.textContent = ` ${value.toUpperCase()} is indeed a palindrome!`;
-      body.style.backgroundColor = "green";
+      body.classList.add("isPalindrome");
     } else {
       result.textContent = `${value.toUpperCase()} is not a palindrome!`;
-      body.style.backgroundColor = "red";
+      body.classList.add("isNotPalindrome");
     }
   } else {
     result.textContent = "Please put a value into the input field";
-    body.style.backgroundColor = "aquamarine";
+    body.classList.add("defaultBackground");
   }
   input.value = "";
+  answer = "";
 });
